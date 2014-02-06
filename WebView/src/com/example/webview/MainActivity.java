30a8e1acc332f4af0157ca069d0a7b9ac1f50ce8
kaito-y <yyhataker407@gmail.com>
2014-02-07 00:50:21 +0900

    WebView
    WebView

---
+++ WebView/src/com/example/webview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.webview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		 WebView web = (WebView)findViewById(R.id.webView);
+	        // httpクライアントを設定
+	        web.setWebViewClient(new WebViewClient());
+	        web.loadUrl("http://www.google.com");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
